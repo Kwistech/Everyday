4fff36543b8a6560f249951e0c664add5ecdbe6b
Johnathon Kwisses <johnkwisses@gmail.com>
2018-08-09 17:18:06 -0600

    Add LogContract interfaces View and Presenter.

--- app/src/main/java/ca/kwisses/everyday/log/LogContract.java
+++ app/src/main/java/ca/kwisses/everyday/log/LogContract.java
@@ -1,4 +1,33 @@
 package ca.kwisses.everyday.log;
 
+import java.util.Date;
+import java.util.List;
+
 public interface LogContract {
+
+    interface Object {
+
+        Log getLog();
+
+        void setLog(Log log);
+
+        List<String> getList();
+
+        void setList(List<String> list);
+
+    }
+
+    interface Presenter {
+
+        void addToLog(List<String> list);
+
+        Log getLog();
+
+        void setLog(Log log);
+
+        boolean isValidListItem(String item);
+
+        Date getCurrentDate();
+
+    }
 }
