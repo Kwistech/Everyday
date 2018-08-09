3a1b0e128e22a36ccdaf86f1d81c9332ad19d8ec
Johnathon Kwisses <johnkwisses@gmail.com>
2018-08-09 17:11:07 -0600

    Add SplashscreenContract interfaces View (which has nothing), and Presenter.

--- app/src/main/java/ca/kwisses/everyday/splashscreen/SplashscreenContract.java
+++ app/src/main/java/ca/kwisses/everyday/splashscreen/SplashscreenContract.java
@@ -1,4 +1,22 @@
 package ca.kwisses.everyday.splashscreen;
 
+import android.content.Context;
+import android.content.Intent;
+
 public interface SplashscreenContract {
+
+    interface View {
+        
+    }
+
+    interface Presenter {
+
+        View getContentView();
+
+        Intent getIntent(Context context);
+
+        void run();
+
+    }
+
 }
