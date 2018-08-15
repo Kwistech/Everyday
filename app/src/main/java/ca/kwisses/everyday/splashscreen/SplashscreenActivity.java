82d1958071e820e8a8fd85666e8032247404f8b8
Johnathon Kwisses <johnkwisses@gmail.com>
2018-08-15 17:19:56 -0600

    Finish Splashscreen Activity java file and start MainActivity layout.

--- app/src/main/java/ca/kwisses/everyday/splashscreen/SplashscreenActivity.java
+++ app/src/main/java/ca/kwisses/everyday/splashscreen/SplashscreenActivity.java
@@ -3,11 +3,18 @@ package ca.kwisses.everyday.splashscreen;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ca.kwisses.everyday.R;
+
 public class SplashscreenActivity extends AppCompatActivity {
+
+    private SplashScreenPresenter splashScreenPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.activity_splashscreen);
+
+        splashScreenPresenter = new SplashScreenPresenter(this);
+        splashScreenPresenter.run();
     }
 }
